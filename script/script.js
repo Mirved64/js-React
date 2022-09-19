137ae685464fa237d5e4f7d87b612b7fa58ef39d
Mirved <medmixal@yahoo.com>
2022-09-19 11:23:50 +0400

    review code

--- script/script.js
+++ script/script.js
@@ -48,7 +48,7 @@ function rememberMyFilms () {
         const a = prompt('Один из последних просмотренных фильмов?'),
               b = prompt('На сколько оцените его?');
     
-        if (a !=null && b != null && a != '' && b !='' && a.length < 50) {
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b; 
             console.log('done');
         } else {
